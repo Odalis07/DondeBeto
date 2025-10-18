4d7753da4bff629fd9fd9adb6d6d69302c4b2968
Odalis07 <orodriguez2689@utm.edu.ec>
2025-10-18 00:12:04 -0500

    Creacion del html y css del apartado de registro y otras correciones

--- DondeBeto/restaurante/urls.py
+++ DondeBeto/restaurante/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import login_view
+from . import views
 
 urlpatterns = [
     path('', login_view, name='login'),
+    path('registro/', views.registro_view, name='registro'),
 ]
--- DondeBeto/restaurante/views.py
+++ DondeBeto/restaurante/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def login_view(request):
     return render(request, 'usuario/login.html')
+
+def registro_view(request):
+    return render(request, 'usuario/registro.html')
+
